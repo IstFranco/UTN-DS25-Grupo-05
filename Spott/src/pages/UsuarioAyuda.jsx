5e1a15edec9d4947e5b63a0b51d781b57bc15318
EstebanTrillo <estebandaniel779@gmail.com>
2025-07-05 21:59:18 -0300

    Agregacion de Componente UsuarioEmpresa en Pages

---
+++ Spott/src/pages/UsuarioAyuda.jsx
@@ -0,0 +1,20 @@
+export default function UsuarioAyuda() {
+    return (
+        <div className="ayuda">
+        <h1>¿Necesitas ayuda?</h1>
+        <p>
+            ¡Bienvenido a nuestra plataforma! Aquí puedes registrarte fácilmente a los
+            eventos que más te interesan y participar sugiriendo canciones para que la
+            música sea de tu agrado.
+        </p>
+
+        <h3>¿Cómo registrarte a un evento?</h3>
+        <ul>
+            <li>Inicia sesión o crea tu cuenta.</li>
+            <li>Navega por la lista de eventos disponibles</li>
+            <li>Haz clic en “Registrar” en el evento que quieras asistir.</li>
+            <li>Recibirás una confirmación por correo electrónico.</li>
+        </ul>
+        </div>
+    );
+}
