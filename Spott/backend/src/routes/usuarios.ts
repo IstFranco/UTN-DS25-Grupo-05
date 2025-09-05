e36d64fb705bd1415630f4d835bb28f18e298e49
LautaroFlores47 <lautaroflores47@gmail.com>
2025-09-05 14:37:31 -0300

    validar usuarios y login

--- Spott/backend/src/routes/usuarios.ts
+++ Spott/backend/src/routes/usuarios.ts
@@ -1,11 +1,13 @@
 // routes/usuarios.ts
 import express from 'express';
 import { crearUsuario, obtenerUsuario, actualizarUsuario, loginUsuario } from '../controllers/usuariosController.js';
+import { validate } from '../middlewares/validate.js';
+import { usuarioRegisterSchema, usuarioLoginSchema } from '../validations/usuariosSchema.js';
 
 const router = express.Router();
 
-router.post('/registro', crearUsuario);
-router.post('/login', loginUsuario);
+router.post('/registro', validate(usuarioRegisterSchema), crearUsuario);
+router.post('/login', validate(usuarioLoginSchema), loginUsuario);
 router.get('/:id', obtenerUsuario);
 router.put('/:id', actualizarUsuario);
 
